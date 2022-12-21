e2c345afe89e6dd596bb8f6b62482439f0af3b60
Pedro André dos Santos <pedro.santos@havan.com.br>
2022-12-21 07:59:51 -0300

    adicionando variantes do AbBotao

--- stories/AbBotao.stories.tsx
+++ stories/AbBotao.stories.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import {ComponentMeta, ComponentStory} from '@storybook/react';
 
-import {AbBotao} from '../src';
+import {AbBotao, AbBotaoProps} from '../src';
 
 export default {
     title: 'Componentes/AbBotao',
     component: AbBotao
 } as ComponentMeta<typeof AbBotao>
 
-const Template: ComponentStory<typeof AbBotao> = () => <AbBotao/> 
+const Template: ComponentStory<typeof AbBotao> = (args) => <AbBotao {...args}/> 
 
 export const Primario = Template.bind({})
+export const Secundario = Template.bind({})
+
+Primario.args = {
+    texto: 'Ab Botão primário',
+    tipo: 'primario'
+} as AbBotaoProps
+
+
+Secundario.args = {
+    texto: 'Ab Botão secundário',
+    tipo: 'secundario'
+} as AbBotaoProps
